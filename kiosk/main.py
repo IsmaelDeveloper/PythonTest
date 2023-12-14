d47d70d10e21dd77e84150c2c8890327dff4c312
ismaelDeveloper <ismael.hadj13@gmail.com>
2023-12-14 17:10:43 +0900

    reorganisation of architechture kiosk

--- kiosk/main.py
+++ kiosk/main.py
@@ -7,52 +7,7 @@ from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtQuick import QQuickView
 from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage, QWebEngineSettings, QWebEngineProfile
 from PyQt5.QtGui import QIcon, QPainter
-
-
-class CustomTabBar(QTabBar):
-    firstIconClicked = pyqtSignal()
-    secondIconClicked = pyqtSignal()
-
-    def __init__(self, parent=None):
-        super(CustomTabBar, self).__init__(parent)
-        self.firstIcon = QIcon("./ressources/images/configure-dark.png")
-        self.secondIcon = QIcon("./ressources/images/refresh.png")
-
-        # Ajouter deux faux onglets pour les icônes
-        self.addTab("")
-        self.addTab("")
-
-    def paintEvent(self, event):
-        super(CustomTabBar, self).paintEvent(event)
-        painter = QPainter(self)
-
-        # Dessiner la première icône
-        firstIconTabRect = self.tabRect(self.count() - 2)
-        firstIconRect = QRect(firstIconTabRect.x(),
-                              firstIconTabRect.y(), 30, 30)
-        self.firstIcon.paint(painter, firstIconRect)
-
-        # Dessiner la seconde icône
-        secondIconTabRect = self.tabRect(self.count() - 1)
-        secondIconRect = QRect(secondIconTabRect.x(),
-                               secondIconTabRect.y(), 30, 30)
-        self.secondIcon.paint(painter, secondIconRect)
-
-    def mousePressEvent(self, event):
-        # Détecter les clics sur la première icône
-        if self.tabRect(self.count() - 2).contains(event.pos()):
-            self.firstIconClicked.emit()
-            event.accept()  # Accepter l'événement pour empêcher le changement d'onglet
-            return  # Ne pas appeler super.mousePressEvent
-
-        # Détecter les clics sur la seconde icône
-        if self.tabRect(self.count() - 1).contains(event.pos()):
-            self.secondIconClicked.emit()
-            event.accept()  # Accepter l'événement pour empêcher le changement d'onglet
-            return  # Ne pas appeler super.mousePressEvent
-
-        # Comportement par défaut pour les autres zones
-        super(CustomTabBar, self).mousePressEvent(event)
+from customTabBar import CustomTabBar
 
 
 class WebEnginePage(QWebEnginePage):
