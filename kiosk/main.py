ab6d31c1a93306fe842cf9a54a1c713bff155051
ismaelDeveloper <ismael.hadj13@gmail.com>
2023-12-14 10:08:25 +0900

    qtimer with qpushbutton for close webview

--- kiosk/main.py
+++ kiosk/main.py
@@ -1,6 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton
-from PyQt5.QtCore import Qt, QUrl, pyqtSignal, pyqtSlot
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel
+from PyQt5.QtCore import Qt, QUrl, pyqtSignal, pyqtSlot, QTimer
 from PyQt5.QtQuickWidgets import QQuickWidget
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtMultimediaWidgets import QVideoWidget
@@ -39,6 +39,12 @@ class MainApp(QWidget):
         self.busClicked.connect(self.onBusClicked)
         self.kioskClicked.connect(self.onKioskClicked)
 
+        # set Timer for close btn
+        self.countdown_timer = QTimer(self)
+        self.countdown_timer.timeout.connect(self.updateCountdown)
+        self.countdown_button = QPushButton("50", self)
+        self.countdown_button.clicked.connect(self.closeWebview)
+
     def initUI(self):
         self.setObjectName("mainWindow")
         self.host = "http://192.168.0.3"
@@ -73,6 +79,7 @@ class MainApp(QWidget):
         self.web_view = QWebEngineView()
 
         self.configureWebEngineSettings()
+
         self.loadStyleSheet()
 
     def loadStyleSheet(self):
@@ -95,6 +102,7 @@ class MainApp(QWidget):
         self.video_widget.setParent(None)
         self.video_widget.hide()
         self.web_view.show()
+        QTimer.singleShot(100, self.setupCountdown)
 
     def openFullScreenWebView(self, url):
         self.clearCache()
@@ -112,6 +120,43 @@ class MainApp(QWidget):
         self.web_view.setUrl(QUrl(url))
         self.web_view.setWindowFlags(Qt.FramelessWindowHint)
         self.web_view.showFullScreen()
+
+    def setupCountdown(self):
+        self.countdown_time = 50
+        self.countdown_button.setText(str(self.countdown_time))
+        self.countdown_button.resize(100, 50)
+        web_view_geometry = self.web_view.geometry()
+        button_x = web_view_geometry.right() - self.countdown_button.width() - 10
+        button_y = web_view_geometry.top() + 10
+        self.countdown_button.move(button_x, button_y)
+        self.countdown_button.raise_()
+        self.countdown_button.show()
+
+        self.countdown_timer.start(1000)
+
+    def updateCountdown(self):
+        self.countdown_time -= 1
+        self.countdown_button.setText(str(self.countdown_time))
+        if self.countdown_time <= 0:
+            self.closeWebview()
+
+    def closeWebview(self):
+        self.countdown_timer.stop()
+        self.countdown_button.hide()
+        self.restoreVideoView()
+        self.web_view.setParent(None)
+        self.web_view.hide()
+
+    def restoreVideoView(self):
+        self.video_widget.setParent(self)
+        main_layout = self.layout()
+        main_layout.addWidget(self.video_widget, 3)
+        self.video_player.setVideoOutput(self.video_widget)
+        self.video_widget.show()
+        self.video_player.stop()
+        self.video_player.setMedia(QMediaContent(QUrl.fromLocalFile(
+            "./ressources/default_media.mp4")))
+        self.video_player.play()
 
     @pyqtSlot()
     def onMediaStatusChanged(self):
