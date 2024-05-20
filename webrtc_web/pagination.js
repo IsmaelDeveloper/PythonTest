eed1b8c7fb8b548acfa9e596467878a0ab641211
ismaelDeveloper <ismael.hadj13@gmail.com>
2024-05-20 10:31:08 +0900

    pagination modified

--- webrtc_web/pagination.js
+++ webrtc_web/pagination.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let totalUsers = allUsers.length;
   const prevUsersBtn = document.getElementById("prevUsers");
   const nextUsersBtn = document.getElementById("nextUsers");
+
   function updateUsersVisibility() {
     allUsers.forEach((user, index) => {
       user.style.display =
@@ -25,14 +26,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
   nextUsersBtn.onclick = function () {
     currentIndex += usersPerPage;
-    if (currentIndex > totalUsers - usersPerPage)
-      currentIndex = totalUsers - usersPerPage;
+    if (currentIndex > totalUsers - 1) currentIndex = totalUsers - 1;
     updateUsersVisibility();
   };
 
   function initializePagination() {
     allUsers = document.querySelectorAll(".user-container");
     totalUsers = allUsers.length;
+    currentIndex = 0; // Reset to the first page on initialization
     updateUsersVisibility();
   }
 
