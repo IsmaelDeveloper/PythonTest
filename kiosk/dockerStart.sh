de8cc6e05ec576f76f63f03e7c90e26ec8e393c8
ismaelDeveloper <ismael.hadj13@gmail.com>
2024-05-21 17:45:56 +0900

    dockerfile and dockerstart

--- kiosk/dockerStart.sh
+++ kiosk/dockerStart.sh
@@ -5,6 +5,13 @@ read -p "사용자 이름을 입력하세요: " USERNAME
 xhost +local:docker
 
 sudo docker run --privileged -it \
-    -e DISPLAY=$DISPLAY -e USERNAME=$USERNAME \
+    -e DISPLAY=$DISPLAY \
+    -e USERNAME=$USERNAME \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    ismaelhadj/kiosk_docker
+    -v /etc/machine-id:/etc/machine-id \
+    -v /run/user/$(id -u)/pulse:/run/user/$(id -u)/pulse \
+    -e PULSE_SERVER=unix:/run/user/$(id -u)/pulse/native \
+    --device /dev/snd \
+    ismaelhadj/kiosk_docker:nozip
+
+
